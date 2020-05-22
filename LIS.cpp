e7674b0408b65b6371e1d3b0fde553c125dcfd26
Shafayat Jamil <shafayatiuc@gmail.com>
2020-05-23 02:14:44 +0600

    Update LIS.cpp

--- LIS.cpp
+++ LIS.cpp
@@ -1,34 +1,56 @@
 #include<bits/stdc++.h>
 using namespace std;
+const int bound=1e9;
+int fx[]={0,-1,-1,-1,0,1,1,1};
+int fy[]={1,1,0,-1,-1,-1,0,1};
+map<pair<int, int>,int>can;
+map<pair<int,int>,int>visited;
+int sr,sc,er,ec,n;
+int rn,st,ed;
+bool flag = false;
+
+void bfs(pair<int,int>start)
+{
+	queue<pair<int,int> >q;
+	q.push(start);
+	visited[start]++;
+	while(!q.empty())
+	{
+		pair<int,int>p;
+		p=q.front();
+		q.pop();
+		for(int i=0;i<8;i++)
+		{
+			int row = p.first+fx[i];
+			int col = p.second+fy[i];
+			if(row<=bound && col<=bound && row>=1 && col>=1 && can[{row,col}]>0 && visited[{row,col}]==0)
+			{
+				if(row ==er && col==ec)
+				{
+					cout<<visited[p]<<endl;
+					flag = true;
+					return;
+				}
+				q.push({row,col});
+				visited[{row,col}] = visited[p]+1;
+				
+			}
+		}
+	}
+}
+
 int main()
 {
-	int test,n,k,num; cin>>test;
-	while(test--)
+	cin>>sr>>sc>>er>>ec;
+	cin>>n;
+	while(n--)
 	{
-		set<int>s;
-		cin>>n>>k;
-		for(int i=0;i<n;i++)
+		cin>>rn>>st>>ed;
+		for(int i=st;i<=ed;i++)
 		{
-			cin>>num;
-			s.insert(num);
+			can[{rn,i}]++;
 		}
-		if((int)s.size() > k)
-		{
-			cout<<"-1"<<endl;
-			continue;
-		}
-		cout<<n*k<<endl;
-		for(int i=0;i<n;i++)
-		{
-			for(auto j:s)
-			{
-				cout<<j<<" ";
-			}
-			for(int q=0;q<k-(int)s.size();q++)
-			{
-				cout<<1<<" ";
-			}
-		}
-		cout<<endl;
 	}
+	bfs({sr,sc});
+	if(!flag) cout<<"-1"<<endl;
 }
